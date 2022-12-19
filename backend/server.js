362dc19a2dd306f2fd3ab1c34b36f91bba9d6e70
unknown <Administrator@BG1NB1223.SONATA.LOCAL>
2022-12-19 13:45:25 +0530

    root path serrve react app

--- backend/server.js
+++ backend/server.js
@@ -17,17 +17,25 @@ app.use(bodyParser.json());
 app.use("/user", require("./routes/userRoutes"));
 
 // Serve frontend
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/build")));
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-  app.get("*", (req, res) =>
-    res.sendFile(
-      path.resolve(__dirname, "../", "frontend", "build", "index.html")
-    )
-  );
-} else {
-  app.get("/", (req, res) => res.send("Please set to production"));
-}
+//   app.get("*", (req, res) =>
+//     res.sendFile(
+//       path.resolve(__dirname, "../", "frontend", "build", "index.html")
+//     )
+//   );
+// } else {
+//   app.get("/", (req, res) => res.send("Please set to production"));
+// }
+
+app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+app.get("*", (req, res) =>
+  res.sendFile(
+    path.resolve(__dirname, "../", "frontend", "build", "index.html")
+  )
+);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
